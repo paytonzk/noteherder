2f0436d2ff8a95f66c3f827808a8e37a00591311
Zach Payton <paytonzk@rose-hulman.edu>
2017-07-06 14:26:55 -0400

    Made clicking put note into form

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -4,6 +4,8 @@ import './NoteForm.css'
 
 class NoteForm extends Component {
   render() {
+    const { currentNote } = this.props
+
     return (
       <div className="NoteForm">
         <div className="form-actions">
@@ -17,11 +19,14 @@ class NoteForm extends Component {
               type="text"
               name="title"
               placeholder="Title your note"
-              setCurrentNote={this.props.setCurrentNote}
+              value={currentNote.title}
             />
           </p>
           
-          <textarea name="body"></textarea>
+          <textarea
+            name="body"
+            value={currentNote.body}
+          ></textarea>
         </form>
       </div>
     )
--- src/NoteList.js
+++ src/NoteList.js
@@ -3,16 +3,19 @@ import React from 'react'
 import './NoteList.css'
 import Note from './Note'
 
-const NoteList = (props) => {
-  const noteIds = Object.keys(props.notes)
+const NoteList = ({ notes, setCurrentNote }) => {
+  const noteIds = Object.keys(notes)
 
   return (
     <div className="NoteList">
       <h3>Notes</h3>
       <ul id="notes">
         {noteIds.map(noteId => (
-          <Note note={props.notes[noteId]} key={noteId}
-          setCurrentNote={props.setCurrentNote} />
+          <Note
+            key={noteId}
+            note={notes[noteId]}
+            setCurrentNote={setCurrentNote}
+          />
         ))}
       </ul>
     </div>
