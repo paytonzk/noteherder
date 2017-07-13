b290f3c807d431bb0e4d1cea81efddae7d555485
Zach Payton <paytonzk@rose-hulman.edu>
2017-07-13 12:26:07 -0400

    Added updateAt field

--- src/Note.js
+++ src/Note.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Note = ({ note }) => {
+  state={
+    updateAt: Date.now
+  }
   return (
     <NavLink to={`/notes/${note.id}`}>
       <li>
